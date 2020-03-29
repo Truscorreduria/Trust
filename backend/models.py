c526ab46b576d4810a56703c9ba70d34956dbb7e
xangcastle <xangcastle@gmail.com>
2020-03-29 17:45:00 -0600

    config mis seguros

--- backend/models.py
+++ backend/models.py
@@ -32,7 +32,7 @@ def get_media_url(model, filename):
 
 
 def get_profile(user):
-    return Cliente.objects.get_or_create(user=user)
+    return Cliente.objects.get(user=user)
 
 
 def get_config(user):
