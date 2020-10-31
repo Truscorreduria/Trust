8f19bf28de1afdc168ceec8d4951aedce7eaf8b2
xangcastle <xangcastle@gmail.com>
2020-10-31 13:16:21 -0600

    mejoras al dashboard

--- frontend/static/trustseguros/js/dashboard.crm.js
+++ frontend/static/trustseguros/js/dashboard.crm.js
@@ -1,4 +1,46 @@
 $(document).ready(function () {
+    const modal = $('#dashboard-modal').iziModal({
+        width: 1200, padding: 20, fullscreen: false, zindex: 1500,
+        headerColor: '#326634'
+    });
+
+
+    const show_data = function () {
+        const data = $(this).data();
+        const content = modal.find('.modal-body').empty();
+        let html = ` <table class="table">
+                        <thead>
+                            <tr>
+                                <td>Número</td>
+                                <td>Prospecto</td>
+                                <td>Campaña</td>
+                                <td class="numberinput">Días</td>
+                                <td class="numberinput">Valor nuevo</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            ${Object.keys(data).map(row => `<tr>
+                                <td>${data[row].code}</td>
+                                <td>${data[row].prospect.name}</td>
+                                <td>${data[row].campain.name}</td>
+                                <td class="numberinput">${data[row].dias}</td>
+                                <td class="numberinput">${data[row].valor_nuevo}</td>
+                            </tr>`).join("")}
+                        </tbody>
+                    </table>`;
+
+        content.html(html);
+        modal.iziModal('open')
+    };
+
+
+    function make_link(data) {
+        return $('<a href="javascript:void(0)"></a>')
+            .text(data.length)
+            .data(data)
+            .on('click', show_data)
+    }
+
     $.ajax('.', {
         method: 'POST',
         data: {crm: 'crm'},
@@ -6,24 +48,23 @@ $(document).ready(function () {
             let sellers = _.groupBy(response.oportunidades, 'vendedor.full_name');
             let crm = $('#dashboard-crm tbody').empty();
             Object.keys(sellers).map(function (o, i) {
-                let status1 = sellers[o].filter(el => el.status.id === 1);
-                let status2 = sellers[o].filter(el => el.status.id === 2);
-                let status3 = sellers[o].filter(el => el.status.id === 3);
-                let status4 = sellers[o].filter(el => el.status.id === 4);
-                let status5 = sellers[o].filter(el => el.status.id === 5);
-                let status6 = sellers[o].filter(el => el.status.id === 6);
-                crm.append(
-                    `<tr>
+                const status1 = make_link(sellers[o].filter(el => el.status.id === 1));
+                const status2 = make_link(sellers[o].filter(el => el.status.id === 2));
+                const status3 = make_link(sellers[o].filter(el => el.status.id === 3));
+                const status4 = make_link(sellers[o].filter(el => el.status.id === 4));
+                const status5 = make_link(sellers[o].filter(el => el.status.id === 5));
+                const status6 = make_link(sellers[o].filter(el => el.status.id === 6));
+                const total = make_link(sellers[o]);
+                crm.append($(`<tr>
                         <td>${o}</td>
-                        <td class="numberinput">${status1.length}</td>
-                        <td class="numberinput">${status2.length}</td>
-                        <td class="numberinput">${status3.length}</td>
-                        <td class="numberinput">${status4.length}</td>
-                        <td class="numberinput">${status5.length}</td>
-                        <td class="numberinput">${status6.length}</td>
-                        <td class="numberinput">${sellers[o].length}</td>
-                    </tr>`
-                )
+                        <td class="numberinput"><a data-swap="status1"></a></td>
+                        <td class="numberinput"><a data-swap="status2"></a></td>
+                        <td class="numberinput"><a data-swap="status3"></a></td>
+                        <td class="numberinput"><a data-swap="status4"></a></td>
+                        <td class="numberinput"><a data-swap="status5"></a></td>
+                        <td class="numberinput"><a data-swap="status6"></a></td>
+                        <td class="numberinput"><a data-swap="total"></a></td>
+                    </tr>`).swapIn({status1, status2, status3, status4, status5, status6, total}))
             });
         }
     })
