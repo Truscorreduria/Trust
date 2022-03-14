e86509e313c30dfe064b059fb5f71d513f78e0fb
César Abel Ramírez <xangcastle@gmail.com>
2022-03-14 16:40:36 +0000

    TC-40

--- frontend/apps/cotizador/views.py
+++ frontend/apps/cotizador/views.py
@@ -1167,7 +1167,7 @@ def invitacion(request):
                        'password': secret}
             context = Context(context)
             html = template.render(context)
-            send_email('Trust correduria te invita a que ingrese a nuestro portal',
+            send_email('Trust correduria le invita a que ingrese a nuestro portal',
                        c.user.email, html)
     return HttpResponseRedirect('/admin/backend/cliente/')
 
